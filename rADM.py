cfd7ef2177a5e5686a26af39f42235b42dbc52d1
Tiago Elias <143190421+tiagoczz@users.noreply.github.com>
2023-11-10 09:27:25 -0300

    Update rADM.py

--- rADM.py
+++ rADM.py
@@ -1,79 +1,106 @@
 #essa def serve para inserir noticias
-def adm_insert_news(all_news, body_news, data_news):
+def adm_insert_news(news, id, usuario):
     while True:
-        news_id = int(input('Crie um ID para a sua notícia:'))
-        email_adm = input('Informe seu e-mail:')
-        for i in all_news.keys():
-            if i == news_id:
-                print('Você não pode informar o mesmo ID.')
-                news_id = int(input('Crie um outro ID:'))
-
-        title_news = input('Insira o título da sua notícia:')
-        all_news[news_id] = [title_news]
-        news = input('Insira o corpo da sua notícia:')
-        body_news[news_id] = [news]
-        data_news[news_id].append(email_adm)
-        yes_or_no = input('Ainda quer inserir notícias? sim/nao')
-        if yes_or_no == 'nao':
+        title = input('Título:')
+        article = input('Corpo da Notícia:')
+        data = input('Insira a data de publicação da notícia no formato dia/mês/ano:')
+        news_id = id[0] + 1
+        id[0] = news_id
+        if usuario in news:
+            news[usuario][news_id] = [title, article, [], 'curtidas', data]
+        else:
+            news[usuario] = {news_id: [title, article, [], 'curtidas', data]}
+        print('\033[92mNotícia adicionada!\033[0m')
+        y = input('Deseja criar mais uma notícia? [sim/nao]')
+        if y == 'nao':
             break
 
 
 #essa def lista todas as notícias
-def adm_list_news(all_news, body_news):
-    print('Essas são todas as notícias.')
-    for key, valor in all_news.items():
-        print(f'ID({key}): {valor[0]}')
-        print(f'{body_news[key]}')
+def adm_list_news(news):
+    for x, y in news.items():
+        print(f'Autor: {x}')
+        for z in y:
+            print(f'Publicado em {y[z][4]}')
+            print(f'Título: {y[z][0]}')
+            print(f'Artigo: {y[z][1]}')
+            comment = ', '. join(y[z][2])
+            print(f'Comentários: {comment}') if comment else ''
+            print(f'{y[z][3]}❤️')
+            print('_'*26)
 
 
 #essa def remove uma notícia
-def adm_remove_news(all_news, body_news):
+def adm_remove_news(news, usuario):
     while True:
-        remove_id = int(input('Informe o ID da notícia para remove-la:'))
-        if remove_id in all_news and remove_id in body_news:
-            all_news.pop(remove_id)
-            body_news.pop(remove_id)
-            yes_or_no = input('Ainda quer remover uma noticia? sim/nao')
-            if yes_or_no == 'nao':
+        x = int(input(
+            '[1]Excluir uma notícia\n'
+            '[2]Excluir todas as notícias\n'
+        ))
+        if x == 1:
+            id_to_remove = int(input('Informe o id da noticia para remove-la:'))
+            if id_to_remove in news[usuario]:
+                news[usuario].pop(id_to_remove)
+                print('\033[92mNotícia removida!\033[0m')
+                y = input('Deseja remover mais notícias? [sim/nao]')
+                if y == 'nao':
+                    break
+            else:
+                print('\033[91mID inexistente!\033[0m')
                 break
-
+        elif x == 2:
+            news.pop(usuario)
+            print('\033[92mTodas as notícia foram removidas!\033[0m')
+            break
         else:
-            print('ID inexistente!')
-            yes_or_no = input('Ainda quer remover uma noticia? sim/nao')
-            if yes_or_no == 'nao':
-                break
+            print('\033[91mOpção inválida!\033[0m')
 
 
-#essa def é para winicius fazer e consertar
-'''def winicius():
+#essa def é para editar uma notícia
+def adm_edit_news(news, usuario):
     while True:
-        edit_id = int(input('Informe o ID da notícia que deseja editar:'))
-        if edit_id in all_news:
-            all_news.pop(edit_id)
-            question = input('Edite a notícia:')
-            all_news[edit_id] = [question]
-            yes_or_no = input('Deseja editar mais alguma coisa? sim/nao')
-            if yes_or_no == 'nao':
+        id_to_edit = int(input('Informe o id da noticia para edita-la:'))
+        if id_to_edit in news[usuario]:
+            junk = int(input(
+                '[1]Editar título\n'
+                '[2]Editar corpo\n'
+                '[3]Editar toda a notícia\n'
+            ))
+            if junk == 1:
+                new_title = input('Novo título:')
+                news[usuario][id_to_edit][0] = new_title
+                print('\033[92mNotícia salva!\033[0m')
+                break
+            elif junk == 2:
+                new_article = input('Novo corpo:')
+                news[usuario][id_to_edit][1] = new_article
+                print('\033[92mNotícia salva!\033[0m')
+                break
+            elif junk == 3:
+                new_title = input('Novo título:')
+                news[usuario][id_to_edit][0] = new_title
+                new_article = input('Novo corpo:')
+                news[usuario][id_to_edit][1] = new_article
+                print('\033[92mNotícia salva!\033[0m')
                 break
             else:
-                print('Não existe nenhuma notícia relacionada a esse ID.')
-                yes_or_no = input('Deseja editar mais alguma coisa? sim/nao')
-                if yes_or_no == 'nao':
-                    break'''
+                print('\033[91mID inexistente!\033[0m')
+                break
+        else:
+            print('\033[91mID inexistente!\033[0m')
 
 
 #essa def serve para buscar notícias
-def adm_search_news(all_news, body_news):
+def adm_search_news(news, usuario):
     while True:
-        search_id = int(input('Informe o ID da notícia para busca-la:'))
-        if search_id in all_news and search_id in body_news:
-            print(all_news[search_id][0])
-            print(body_news[search_id])
-            yes_or_no = input('Deseja continuar procurando? sim/nao')
-            if yes_or_no == 'nao':
-                break
+        id_to_search = int(input('Informe o id da noticia para busca-la:'))
+        if id_to_search not in news[usuario]:
+            print('\033[91mID inexistente!\033[0m')
         else:
-            print('Digite algo válido')
-            yes_or_no = input('Deseja continuar procurando? sim/nao')
-            if yes_or_no == 'nao':
-                break
+            for x, y in news.items():
+                print(f'\033[96mAutor: {x}\033[0m')
+                for z in y:
+                    print(f'Título: {y[z][0]}')
+                    print(f'Artigo: {y[z][1]}')
+                    print('_' * 26)
+        break
