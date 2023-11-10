c9cb30d3a556ce14cd46972b2c5628aaf8f7012d
Tiago Elias <143190421+tiagoczz@users.noreply.github.com>
2023-11-10 09:28:50 -0300

    Update register_adm.py

--- register_adm.py
+++ register_adm.py
@@ -1,17 +1,20 @@
-def register_adm(everybody, passwords):
+def register_adm(users, perfil_adm):
     name = input('Nome:')
-    usuario = input('Crie um usuário:')
-    if usuario in everybody['adms'] or usuario in everybody['users']:
-        print('Usuario existente, cadastre outro')
-    else:
-        everybody['adms'].append(usuario)
-        cpf = int(input('CPF:'))
-        if len(str(cpf)) == 11:
-            password = int(input('Crie uma senha:'))
-            if len(str(password)) > 4:
-                passwords['password_adms'].append(password)
-                print('Cadastro concluído.')
+    email = input('E-mail:')
+    cpf = int(input('CPF:'))
+    if len(str(cpf)) == 11:
+        while True:
+            usuario = input('Crie um usuário:')
+            if usuario not in users:
+                password = input('Crie uma senha:')
+                if len(str(password)) >= 4:
+                    users[usuario] = [password, '1']
+                    perfil_adm[usuario] = [name, email]
+                    print('\033[92mCadastro Concluído.\033[0m')
+                    break
+                else:
+                    print('\033[91mSenha deve ter pelo menos 4 dígitos!\033[0m')
             else:
-                print('A senha precisa possuir mais de 4 dígitos.')
-        else:
-            print('CPF inválido.')
+                print('\033[91mUsuário em uso. Tente outro!\033[0m')
+    else:
+        print('\033[91mCPF inválido.\033[0m')
