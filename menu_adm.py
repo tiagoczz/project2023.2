b833ba6330f7f330a9af8a8b0b5a195992a81af1
Tiago Elias <143190421+tiagoczz@users.noreply.github.com>
2023-11-10 09:25:38 -0300

    Update menu_adm.py

--- menu_adm.py
+++ menu_adm.py
@@ -1,50 +1,36 @@
 from rADM import *
 
 
-def menu_adm(all_news, body_news, data_news):
+def menu_adm(news, id, usuario):
     while True:
         print('_' * 26)
         print(
-            '[1]Inserir notícia\n'
-            '[2]Listar notícias\n'
-            '[3]Excluir notícia\n'
-            '[4]Editar notícia\n'
-            '[5]Buscar notícia\n'
+            '[1]Inserir\n'
+            '[2]Listar\n'
+            '[3]Excluir\n'
+            '[4]Editar\n'
+            '[5]Buscar\n'
             '[6]Logout'
         )
         print('_' * 26)
         choice = input()
         if choice == '1':
-            adm_insert_news(all_news, body_news, data_news)
+            adm_insert_news(news, id, usuario)
 
         elif choice == '2':
-            adm_list_news(all_news, body_news)
+            adm_list_news(news)
 
         elif choice == '3':
-            adm_remove_news(all_news, body_news)
+            adm_remove_news(news, usuario)
 
-        # RESOLVA ISSO WINICIUS
         elif choice == '4':
-            while True:
-                edit_id = int(input('Informe o ID da notícia que deseja editar:'))
-                if edit_id in all_news:
-                    all_news.pop(edit_id)
-                    question = input('Edite a notícia:')
-                    all_news[edit_id] = [question]
-                    yes_or_no = input('Deseja editar mais alguma coisa? sim/nao')
-                    if yes_or_no == 'nao':
-                        break
-                else:
-                    print('Não existe nenhuma notícia relacionada a esse ID.')
-                    yes_or_no = input('Deseja editar mais alguma coisa? sim/nao')
-                    if yes_or_no == 'nao':
-                        break
+            adm_edit_news(news, usuario)
 
         elif choice == '5':
-            adm_search_news(all_news, body_news)
+            adm_search_news(news, usuario)
 
         elif choice == '6':
             break
 
         else:
-            print('Não existe esse opção.')
+            print('Opção inválida.')
