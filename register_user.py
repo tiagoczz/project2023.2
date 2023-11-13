e13f0faeaaa80ba258987b8061097cad3dfc5cff
Tiago Elias <143190421+tiagoczz@users.noreply.github.com>
2023-11-13 16:49:24 -0300

    Update register_user.py

--- register_user.py
+++ register_user.py
@@ -4,10 +4,11 @@ def register_user(users):
     while True:
         usuario = input('Crie um usuário:')
         if usuario not in users:
-            password = int(input('Crie uma senha:'))
+            password = input('Crie uma senha:')
             if len(str(password)) > 4:
                 users[usuario] = [password, '2']
                 print('\033[92mCadastro concluído.\033[0m')
+                break
             else:
                 print('\033[91mSenha deve ter pelo menos 4 dígitos!\033[0m')
         else:
