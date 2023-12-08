9ed60c385f17d01c36cefe87e0c0e4ec0789028b
Tiago Elias <143190421+tiagoczz@users.noreply.github.com>
2023-12-08 10:26:09 -0300

    Update rUSER.py

--- rUSER.py
+++ rUSER.py
@@ -1,99 +1,87 @@
-#def usada na def user_search_news
-def comment(news, id_for_add_comment):
+#def usada na def user_search_news, user_comment_news, user_like_news
+def search(news):
+    found = False
+    search = input('Pesquisa:')
+    for autor, dicionario in news.items():
+        for id_news, lista in dicionario.items():
+            if search in lista[0]:
+                found = True
+                return found, autor, lista, id_news
+
+    if not found:
+        print('Nenhuma notícia encontrada!')
+        return found, None, None, None
+
+
+#def usada na def user_search_news e na def user_comment_news
+def comment(news, autor, id_news):
     comentario = input('Comentário:')
-    for x, y in news.items():
-        if id_for_add_comment in y:
-            news[x][id_for_add_comment][2].append(comentario)
-            print('\033[92mComentário adicionado\033[0m')
+    news[autor][id_news][2].append(comentario)
+    print('\033[92mComentário adicionado\033[0m')
 
 
 #essa def serve para o usuário buscar uma noticia
 def user_search_news(news):
-    for a, b in news.items():
-        for c, d in b.items():
-            print(f"Autor: {a} \nID: {c} \nTítulo: {d[0]}")
-            print('_' * 26)
-
-    while True:
-        found = False
-        id_to_search = int(input('Informe o ID da notícia para buscá-la:'))
-        for x, y in news.items():
-            if id_to_search in y:
-                print(f'\033[96mAutor: {x}\033[0m')
-                print(f'Título: {y[id_to_search][0]}')
-                print(f'Artigo: {y[id_to_search][1]}')
-                print('_' * 26)
-                found = True
-                comment_question = input('Deseja fazer um comentário? [sim/nao]')
-                if comment_question == 'sim':
-                    comment(news, id_to_search)
-                    junk = input('Deseja fazer outro comentário? [sim/nao]')
-                    if junk == 'sim':
-                        comment(news, id_to_search)
-
-        if not found:
-            print('\033[91mID inexistente!\033[0m')
-
-        break
+    x, autor, lista, id_news = search(news)
+    if x:
+        print(f'Publicado em {lista[4]}')
+        print(f'Autor: {autor}')
+        print(f'Título: {lista[0]}')
+        print(f'Artigo: {lista[1]}')
+        comentario = ', '.join(lista[2])
+        print(f'Comentários: {comentario}')
+        print(f'{len(lista[3])}❤️')
+        add_comment = input('Deseja fazer um comentário? [sim/nao]')
+        if add_comment == 'sim':
+            comment(news, autor, id_news)
 
 
 #essa def serve para o usuário comentar na notícia
 def user_comment_news(news):
-    for a, b in news.items():
-        for c, d in b.items():
-            print(f"Autor: {a} \nID: {c} \nTítulo: {d[0]}")
-            print('_' * 26)
+    comment_more = True
+    while comment_more:
+        x, autor, lista, id_news = search(news)
+        if x:
+            comment(news, autor, id_news)
+        else:
+            break
 
-    while True:
-        found = False
-        comment_id = int(input('Informe o ID da notícia para comentar:'))
-        comentario = str(input('Comentário:'))
-        for x, y in news.items():
-            if comment_id in y:
-                news[x][comment_id][2].append(comentario)
-                print('\033[92mComentário adicionado\033[0m')
-                found = True
-                junk = input('Deseja fazer outro comentário? [sim/nao]')
-                if junk == 'sim':
-                    comment(news, comment_id)
-
-        if not found:
-            print('\033[91mID inexistente!\033[0m')
-
-        break
+        other_comment = input('Deseja fazer mais um comentário? [sim/nao]')
+        if other_comment == 'nao':
+            comment_more = False
 
 
 #essa def serve para o usuário curtir a notícia
 def user_like_news(news):
-    for a, b in news.items():
-        for c, d in b.items():
-            print(f"Autor: {a} \nID: {c} \nTítulo: {d[0]}")
-            print('_' * 26)
-
     while True:
-        found = False
-        like_id = int(input('Informe o ID da noticia para curti-la:'))
-        for x, y in news.items():
-            if like_id in y:
-                news[x][like_id][3].append('❤️')
-                print('\033[92mCurtida adicionada!\033[0m')
-                found = True
+        x, autor, lista, id_news = search(news)
+        if x:
+            news[autor][id_news][3].append('❤️')
+            print('\033[92mCurtida adicionada!\033[0m')
 
-        if not found:
-            print('\033[91mID inexistente!\033[0m')
-
-        break
+        other_like = input('Deseja curtir mais alguma noticia? [sim/nao]')
+        if other_like == 'nao':
+            break
 
 
 #essa def serve para o usuário listar as notícias
 def user_list_news(news):
     for x, y in news.items():
-        print(f'Autor: {x}')
         for z in y:
+            print(f'Autor: {x}')
             print(f'Publicado em {y[z][4]}')
             print(f'Título: {y[z][0]}')
             print(f'Artigo: {y[z][1]}')
-            a = ', '.join(y[z][2])
-            print(f'Comentários: {a}') if comment else ''
+            comentario = ', '.join(y[z][2])
+            print(f'Comentários: {comentario}') if comentario else ''
             print(f'{len(y[z][3])}❤️')
             print('_' * 26)
+
+
+#essa def serve para favoritar um autor/adm
+def user_favorite_author(usuario, perfis):
+    email_user = perfis[usuario][2]
+    author_user = input('Informe o nome de usuário do autor que você deseja adicionar aos favoritos.')
+    if author_user in perfis and perfis[author_user][0] == '1':
+        perfis[author_user][3].append(email_user)
+        print('\033[92mAutor favoritado!\033[0m')
