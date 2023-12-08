783b29127d5141ef9c27f651e2b65f8adb4e7b72
Tiago Elias <143190421+tiagoczz@users.noreply.github.com>
2023-12-08 10:25:05 -0300

    Update menu_adm.py

--- menu_adm.py
+++ menu_adm.py
@@ -1,7 +1,7 @@
 from rADM import *
 
 
-def menu_adm(news, id, usuario):
+def menu_adm(news, id, usuario, my_news_rank, geral_news_rank, list_download_news, perfis):
     while True:
         print('_' * 26)
         print(
@@ -10,15 +10,18 @@ def menu_adm(news, id, usuario):
             '[3]Excluir\n'
             '[4]Editar\n'
             '[5]Buscar\n'
-            '[6]Logout'
+            '[6]Meu Ranking\n'
+            '[7]Ranking geral\n'
+            '[8]Baixar notícias\n'
+            '[9]Logout'
         )
         print('_' * 26)
         choice = input()
         if choice == '1':
-            adm_insert_news(news, id, usuario)
+            adm_insert_news(news, id, usuario, perfis)
 
         elif choice == '2':
-            adm_list_news(news)
+            adm_list_news(news, usuario)
 
         elif choice == '3':
             adm_remove_news(news, usuario)
@@ -30,6 +33,15 @@ def menu_adm(news, id, usuario):
             adm_search_news(news)
 
         elif choice == '6':
+            adm_rank_news(news, usuario, my_news_rank)
+
+        elif choice == '7':
+            adm_geral_news_rank(news, geral_news_rank)
+
+        elif choice == '8':
+            adm_download_news(news, list_download_news, usuario)
+
+        elif choice == '9':
             break
 
         else:
