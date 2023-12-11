7d66b0d86563e2b53fb41f1c72e44d21d80bb564
Tiago Elias <143190421+tiagoczz@users.noreply.github.com>
2023-12-11 14:52:21 -0300

    Update login.py

--- login.py
+++ login.py
@@ -4,11 +4,14 @@ def login_adm_user(users):
     tipo = 0
     usuario = input('Usuário:')
     password = int(input('Senha:'))
-
+    #verifica se existe o usuario e a senha informados no dicionario users
     if usuario in users and password == users[usuario][0]:
+        #se no dicionario users o indice 1 da lista for igual a 1, ele é um adm
         if users[usuario][1] == '1':
             tipo = 1
+        #senao ele é um usuario
         else:
             tipo = 2
 
+    #retorna o tipo e o nome de usuario
     return tipo, usuario
