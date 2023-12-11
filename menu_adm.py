e1c7860a075577df22864d3da0a692ae8eed887b
Tiago Elias <143190421+tiagoczz@users.noreply.github.com>
2023-12-11 14:52:42 -0300

    Update menu_adm.py

--- menu_adm.py
+++ menu_adm.py
@@ -16,33 +16,37 @@ def menu_adm(news, id, usuario, my_news_rank, geral_news_rank, list_download_new
             '[9]Logout'
         )
         print('_' * 26)
-        choice = input()
-        if choice == '1':
-            adm_insert_news(news, id, usuario, perfis)
+        try:
+            choice = int(input())
+            if choice == 1:
+                adm_insert_news(news, id, usuario, perfis)
 
-        elif choice == '2':
-            adm_list_news(news, usuario)
+            elif choice == 2:
+                adm_list_news(news, usuario)
 
-        elif choice == '3':
-            adm_remove_news(news, usuario)
+            elif choice == 3:
+                adm_remove_news(news, usuario)
 
-        elif choice == '4':
-            adm_edit_news(news, usuario)
+            elif choice == 4:
+                adm_edit_news(news, usuario)
 
-        elif choice == '5':
-            adm_search_news(news)
+            elif choice == 5:
+                adm_search_news(news)
 
-        elif choice == '6':
-            adm_rank_news(news, usuario, my_news_rank)
+            elif choice == 6:
+                adm_rank_news(news, usuario, my_news_rank)
 
-        elif choice == '7':
-            adm_geral_news_rank(news, geral_news_rank)
+            elif choice == 7:
+                adm_geral_news_rank(news, geral_news_rank)
 
-        elif choice == '8':
-            adm_download_news(news, list_download_news, usuario)
+            elif choice == 8:
+                adm_download_news(news, list_download_news, usuario)
 
-        elif choice == '9':
-            break
+            elif choice == 9:
+                break
 
-        else:
-            print('Opção inválida.')
+            else:
+                print('\033[91mOpção inválida!\033[0m')
+
+        except ValueError:
+            print('\033[91mOpção inválida!\033[0m')
