ce859ba26a0815fd5914bbe78e1a1381e9465b4c
Tiago Elias <143190421+tiagoczz@users.noreply.github.com>
2023-12-11 14:55:21 -0300

    Update register_adm.py

--- register_adm.py
+++ register_adm.py
@@ -1,4 +1,5 @@
-#essa def serve para registrar um adm
+#tipo = 1(adm), tipo = 2(usuário) e tipo = 0(nada)
+#def usada cadastrar um adm
 def register_adm(users, perfis):
     name = input('Nome:')
     email = input('E-mail:')
@@ -6,10 +7,14 @@ def register_adm(users, perfis):
     if len(str(cpf)) == 11:
         while True:
             usuario = input('Crie um usuário:')
+            # verifica se o usuario criado existe no dicionario users
             if usuario not in users:
                 password = int(input('Crie uma senha:'))
                 if len(str(password)) >= 4:
+                # cria uma nova chave(usuario) no dicionario users, e cria uma lista como valor contendo a senha e o tipo ='1'
                     users[usuario] = [password, '1']
+                # cria uma nova chave(usuario) no dicionario perfis, e cria uma lista como valor contendo tipo, o nome,
+                #o email e uma lista vazia que ira guardar os email dos usuarios que favoritaram esse autor
                     perfis[usuario] = ['1', name, email, []]
                     print('\033[92mCadastro Concluído.\033[0m')
                     break
